35a4c50d119bea3de5320e8cde465d3f9af8f009
Camile Stefany <camilestefanypereira12@gmail.com>
2024-11-19 13:55:25 -0300

    feat: (src\controllers\ExerciseController.ts  e  prisma\schema.prisma) Criamos verificações de erros no controller e retiramos o valor Unique da variável itemStatus do model Progress. @wander06 e @camsste

--- src/controllers/ExerciseController.ts
+++ src/controllers/ExerciseController.ts
@@ -7,8 +7,25 @@ export class ExerciseController {
         const { id } = req.params;
         const { value } = req.body;
         try {
+              if (!value){
+                return res.status(400).json({message: "Status value is required"});
+              }
+
+              const validateStatus = ["NotStarted", "InProgress", "Completed"]
+              if (!validateStatus.includes(value)){
+                return res.status(400).json({message: "Status value is invalid"});
+              }
+
+            const currentProgress = await prisma.progress.findUnique({ 
+                where: {itemId: id}       
+            })
+
+              if (currentProgress && currentProgress.itemStatus == value){
+                return res.status(400).json({message: "Status value is already being used"});
+              }
+
             const exercise = await prisma.progress.update({where: {itemId : id}, data: { itemStatus: value}});
-            res.status(200).json({message: exercise})
+            res.status(200).json(exercise)
 
         } catch (error) {
             res.json({message: "Erro"})
