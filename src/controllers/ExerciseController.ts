317b36c584c0f1aabe64bd35daa5d031bf806517
Camile Stefany <camilestefanypereira12@gmail.com>
2024-11-19 15:56:46 -0300

    feat: (src\controllers\ExerciseController.ts) Alterando as regras do controller. @Wander06 e @camsste

--- src/controllers/ExerciseController.ts
+++ src/controllers/ExerciseController.ts
@@ -2,33 +2,33 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { Response, Request } from "express";
 
 export class ExerciseController {
-    async updateExerciseStatus (req: Request, res: Response) {
+    async updateExerciseStatus(req: Request, res: Response) {
         const prisma = new PrismaClient();
-        const { id } = req.params;
-        const { value } = req.body;
+        const { exerciseId } = req.params;
+        const { value, userId } = req.body;
         try {
-              if (!value){
-                return res.status(400).json({message: "Status value is required"});
-              }
+            if (!value) {
+                return res.status(400).json({ message: "Status value is required" });
+            }
 
-              const validateStatus = ["NotStarted", "InProgress", "Completed"]
-              if (!validateStatus.includes(value)){
-                return res.status(400).json({message: "Status value is invalid"});
-              }
+            const validateStatus = ["NotStarted", "InProgress", "Completed"]
+            if (!validateStatus.includes(value)) {
+                return res.status(400).json({ message: "Status value is invalid" });
+            }
 
-            const currentProgress = await prisma.progress.findUnique({ 
-                where: {itemId: id}       
+            const currentProgress = await prisma.progress.findUnique({
+                where: { userId: userId, itemId: exerciseId }
             })
 
-              if (currentProgress && currentProgress.itemStatus == value){
-                return res.status(400).json({message: "Status value is already being used"});
-              }
+            if (currentProgress && currentProgress.itemStatus == value) {
+                return res.status(400).json({ message: "Status value is already being used" });
+            }
 
-            const exercise = await prisma.progress.update({where: {itemId : id}, data: { itemStatus: value}});
+            const exercise = await prisma.progress.update({ where: { userId: currentProgress?.userId, itemId: currentProgress?.itemId }, data: { itemStatus: value } });
             res.status(200).json(exercise)
 
         } catch (error) {
-            res.json({message: "Erro"})
+            res.json({ message: "Erro" })
         }
     }
 }
