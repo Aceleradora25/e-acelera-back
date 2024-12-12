fce7dcc3bce3b522fa31593673f0f13e52c6794e
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-12-12 14:44:06 -0300

    fix (src/routes/index.ts) Retirando comentários e rota antiga @Geoziihdev @GuusRodrigues

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,10 +7,9 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.send('Welcome to the homepage');
 });
-//Rota antiga para teste//
-router.put('/exercise/:itemId/status', new ExerciseController().updateExerciseStatus);
-//Rota Nova para salvar progresso//
+
 router.put('/topic/:topicId/item/:itemId/status', validateTokenMiddleware, (req, res) => new ExerciseController().updateExerciseStatus(req, res));
+
 router.get('/topic/:topicId/item', validateTokenMiddleware, (req, res) => new ExerciseController().getTopicExercisesStatus(req, res))
 export default router;
 
