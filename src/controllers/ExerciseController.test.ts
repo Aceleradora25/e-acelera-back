6e9b9bfa6d4c926ca20452ccfebbf679f60223ea
Wanderson Silva <wandersonsilva@Wandersons-MacBook-Pro.local>
2025-01-09 14:37:34 -0300

    fix(src/controllers/ExerciseController.test.ts): Criando testes para updateExerciseStatus - @Wander06 @Lib29bm @geoziihdev

--- src/controllers/ExerciseController.test.ts
+++ src/controllers/ExerciseController.test.ts
@@ -27,14 +27,14 @@ describe('ExerciseController - updateExerciseStatus', () => {
         controller = new ExerciseController()
         controller['exerciseService'] = mockExerciseService
 
-        req = { params: { topicId: "1", itemId: "1" }, body: { ItemStatus: "NotStarted" }, user: { email: "test@gmail.com" } }
+        req = { params: { topicId: "1", itemId: "1" }, body: { itemStatus: "NotStarted" }, user: { email: "test@gmail.com" } }
         res = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn()
         }
     })
 
-    it('Usuário não autenticado', async () => {
+    it('deve retornar "User not authenticated" se o usuário não estiver autenticado', async () => {
         req.user = undefined
 
         await controller.updateExerciseStatus(req as Request, res as Response)
@@ -43,13 +43,78 @@ describe('ExerciseController - updateExerciseStatus', () => {
         expect(res.json).toHaveBeenCalledWith({ message: "User not authenticated." })
     })
 
-    it('Usuário não encontrado', async () => {
+    it('deve retornar "User not found" se o usuário não for encontrado', async () => {
+        mockExerciseService.findUserByEmail.mockResolvedValue(null)
         
         await controller.updateExerciseStatus(req as Request, res as Response)
 
         expect(res.status).toHaveBeenCalledWith(404)
         expect(res.json).toHaveBeenCalledWith({ message: "User not found." })
+    })
+
+    it('deve retornar "Invalid or missing status value" se o status for inválido', async () => {
+        mockExerciseService.findUserByEmail.mockResolvedValue({ id: 1, email: "teste@gmail.com" });
+
+        mockExerciseService.validateStatus.mockResolvedValue(false)
         
+        await controller.updateExerciseStatus(req as Request, res as Response)
+
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ message: "Invalid or missing status value." })
+    })
+
+    it('deve retornar "Progress record not found for user 1 and item 1, 1" se o progresso não for encontrado', async () => {
+        mockExerciseService.findUserByEmail.mockResolvedValue({ id: 1, email: "teste@gmail.com" });
+
+        mockExerciseService.validateStatus.mockResolvedValue(true)
+
+        mockExerciseService.findProgress.mockRejectedValue(new Error("Progress record not found for user 1 and item 1, 1."));
+        
+        await controller.updateExerciseStatus(req as Request, res as Response)
+
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ message: "Progress record not found for user 1 and item 1, 1." })
+    })
+
+    it('deve retornar "The status is already up to date" se o status já estiver atualizado', async () => {
+
+        mockExerciseService.findUserByEmail.mockResolvedValue({ id: 1, email: "teste@gmail.com" });
+
+        mockExerciseService.validateStatus.mockResolvedValue(true)
+
+        mockExerciseService.findProgress.mockResolvedValue({ id: 1, itemId: "rw12346789", itemStatus: "NotStarted", elementType: "Exercise", topicId: "rw987654321", userId: 1 });
+
+        await controller.updateExerciseStatus(req as Request, res as Response)
+
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({ message: "The status is already up to date." })
+    })
+
+    it('deve retornar o progresso atualizado ao mudar o status', async () => {
+        mockExerciseService.findUserByEmail.mockResolvedValue({ id: 1, email: "teste@gmail.com" });
+
+        mockExerciseService.validateStatus.mockResolvedValue(true);
+
+        mockExerciseService.findProgress.mockResolvedValue({ id: 1, itemId: "rw12346789", itemStatus: "InProgress", elementType: "Exercise", topicId: "rw987654321", userId: 1 });
+
+        mockExerciseService.updatedProgress.mockResolvedValue([{ id: 1, itemId: "rw12346789", itemStatus: "NotStarted", elementType: "Exercise", userId: 1 }]);
+
+        await controller.updateExerciseStatus(req as Request, res as Response);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith([{ id: 1, itemId: "rw12346789", itemStatus: "NotStarted", elementType: "Exercise", userId: 1 }]);
+    });
+
+    it('deve retornar "Internal server error while processing the request" em caso de erro no servidor', async () => {
+        mockExerciseService.findUserByEmail.mockResolvedValue({ id: 1, email: "teste@gmail.com" });
+        mockExerciseService.validateStatus.mockResolvedValue(true)
+        
+        mockExerciseService.findProgress.mockRejectedValue(new Error("Internal server error"));
+    
+        await controller.updateExerciseStatus(req as Request, res as Response)
+    
+        expect(res.status).toHaveBeenCalledWith(500)
+        expect(res.json).toHaveBeenCalledWith({ message: "Internal server error while processing the request." })
     })
 
 })
