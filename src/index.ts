44dd17d09481a7278591c1a1dd3a034d4f7b912d
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2025-01-23 15:37:01 -0300

    fix(src/index.ts): apagando comentários @Geoziihdev @stephanybrazeir0

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import express from 'express'
 import router from './routes/index'
 import cors from 'cors'
 
-// const cors = cors()
 const app = express()
 const port = 5002
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
@@ -17,11 +22,4 @@ app.use('/user', router)
 
 app.use('/login', router)
 
-app.use(cors()); 
 
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-.
