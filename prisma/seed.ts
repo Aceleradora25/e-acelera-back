64d73cf96d2d620e0bbe5ddfe16519a5c7d52d71
Geovana Santos <140776038+Geoziihdev@users.noreply.github.com>
2025-03-14 13:31:00 -0300

    Refactor:(prisma/seed.ts src/mocks/userMock.ts) Criamos a pasta mocks e o arquivo userMocks.ts e também refatoramos seed.ts @Geoziihdev @Lip29bm @stephanybrazeir0

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,61 +1,10 @@
 import { PrismaClient } from "@prisma/client"
+import {mockedUser} from"../src/mocks/userMock"
 
 const prisma = new PrismaClient()
 
 async function main() {
-  const user1 = await prisma.user.create({
-    data: {
-      email: "usuariaaceleradora@gmail.com",
-      provider: "google",
-      loginDate: new Date(),
-      Progress: {
-        create: [
-          {
-            itemId: "rw17169056027059821dc",
-            itemStatus: "InProgress",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw1716905602727ecf72e",
-            itemStatus: "Completed",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw17256421537956df97b",
-            itemStatus: "Completed",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw1725642159329a2d9f4",
-            itemStatus: "Completed",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw172564217048038aaf4",
-            itemStatus: "Completed",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw1725642166308a69249",
-            itemStatus: "Completed",
-            elementType: "Exercise",
-            topicId: "rw1721236357157ccf465",
-          },
-          {
-            itemId: "rw1725630408780f28e7a",
-            itemStatus: "Completed",
-            elementType: "Video",
-            topicId: "rw1721236357157ccf465",
-          },
-        ],
-      },
-    },
-  })
+  const user = await prisma.user.create({data: mockedUser})
 }
 
 main()
