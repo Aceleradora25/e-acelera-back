b464a1768f4c7da56804a151e0822af0ae1092fb
Felipe <felipe-bmachado@educar.rs.gov.br>
2025-04-28 10:49:23 -0300

    feat: aprimoramos verificacoes para permitir rotas sem origem mas exigir um authorization @Lip29bm @Geoziihdev

--- src/middleware/corsMiddleware.ts
+++ src/middleware/corsMiddleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express"
 import {isString, isAllowedOrigin, sendCorsResponse} from "../utils/corsUtils"
+import { STATUS_CODE } from "../utils/constants"
 
 export function corsMiddleware(
   req: Request,
@@ -7,6 +8,16 @@ export function corsMiddleware(
   next: NextFunction
 ): void {
   const origin = req?.headers.origin
+
+  if (!origin) {
+    const authHeader = req.headers.authorization
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      next()
+      return
+    }
+    res.status(STATUS_CODE.FORBIDDEN).json({ message: "NOT ALLOWED" })
+    return
+  }
 
   if (isString(origin) && isAllowedOrigin(origin)) {
     sendCorsResponse({ origin, res, next, allowed: true })
