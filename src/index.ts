2f0f5766cf3056ac8d034c036018952c8a7d53bf
Peu Silva <peusilva@proton.me>
2025-09-12 13:37:51 -0300

    feat: error middleware and error classes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
+import "reflect-metadata";
 import express from "express";
 import router from "./routes/index";
+import { errorHandlerMiddleware } from "./middleware/errorHandlerMiddleware";
 
 const app = express();
 const port = 5002;
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 app.use((req, res) => {
   res.status(404).json({
     status: 404,
     error: "Not Found",
-    message: "A rota que você tentou acessar não existe."
+    message: "A rota que você tentou acessar não existe.",
   });
 });
 
