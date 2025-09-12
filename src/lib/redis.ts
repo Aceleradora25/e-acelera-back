e3120787e64b8c4f9943412b173e89fdd6e38bf6
Peu Silva <peusilva@proton.me>
2025-09-12 13:46:12 -0300

    refactor: check if the upstash url and token envs are set, instead of only cache enabled env var

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,3 +1,8 @@
 import { Redis } from "@upstash/redis";
 
-export const redis = Redis.fromEnv();
+export const redis =
+  process.env.IS_CACHE_ENABLED === "TRUE" &&
+  process.env.UPSTASH_REDIS_REST_URL &&
+  process.env.UPSTASH_REDIS_REST_TOKEN
+    ? Redis.fromEnv()
+    : null;
