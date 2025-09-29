d05fe58123d1797704ae7d1008e9387ee5216c43
Peu Silva <peusilva@proton.me>
2025-09-29 17:02:58 -0300

    fix: wrong env variable being used for cache enablement

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,4 +1,4 @@
 import { Redis } from "@upstash/redis";
+import { IS_CACHE_ENABLED } from "../utils/constants";
 
-export const redis =
-  process.env.IS_CACHE_ENABLED === "TRUE" ? Redis.fromEnv() : null;
+export const redis = IS_CACHE_ENABLED ? Redis.fromEnv() : null;
