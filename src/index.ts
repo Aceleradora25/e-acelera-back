7694f48db161c1420709b179723e0d250391c077
Pedro Idiarte Alves <pedroidiarte7@gmail.com>
2025-10-18 02:17:14 -0300

    fix prisma import

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,9 @@ import Connect from 'connect-pg-simple'
 import session from 'express-session'
 
 import { Database, Resource, getModelByName } from '@adminjs/prisma'
-import { PrismaClient } from '@prisma/client'
+import prisma from '../client';
 
 const PORT = 5002;
-const prisma = new PrismaClient()
 AdminJS.registerAdapter({ Database, Resource })
 
 const DEFAULT_ADMIN = {
@@ -38,7 +37,6 @@ const start = async () => {
     },
     ],
   }
-
 
   const app = express();
   const admin = new AdminJS(adminOptions);
