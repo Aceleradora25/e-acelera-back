adcfd79ee38f08ca3285002cb249b4bff454359c
Peu Silva <peusilva@proton.me>
2025-12-09 16:12:09 -0300

    chore: use path alias import on errors

--- src/errors/StackbyErrors.ts
+++ src/errors/StackbyErrors.ts
@@ -1,25 +1,25 @@
-import { AppError } from "./AppError";
+import { AppError } from "@/errors/AppError.js";
 
 export class StackbyFilterError extends AppError {
-  constructor(message: string, details?: unknown) {
-    super(message, 400, details);
-  }
+	constructor(message: string, details?: unknown) {
+		super(message, 400, details);
+	}
 }
 
 export class UnsupportedOperatorError extends StackbyFilterError {
-  constructor(operator: string) {
-    super(`Unsupported operator: ${operator}`, { operator });
-  }
+	constructor(operator: string) {
+		super(`Unsupported operator: ${operator}`, { operator });
+	}
 }
 
 export class MissingValueError extends StackbyFilterError {
-  constructor(operator: string) {
-    super(`Missing value for operator: ${operator}`, { operator });
-  }
+	constructor(operator: string) {
+		super(`Missing value for operator: ${operator}`, { operator });
+	}
 }
 
 export class MissingColumnError extends StackbyFilterError {
-  constructor(operator: string) {
-    super(`Missing column for operator: ${operator}`, { operator });
-  }
+	constructor(operator: string) {
+		super(`Missing column for operator: ${operator}`, { operator });
+	}
 }
