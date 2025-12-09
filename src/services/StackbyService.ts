c3d93af460a3b2062d8a1b9076043fd00525178c
Peu Silva <peusilva@proton.me>
2025-12-09 16:12:27 -0300

    chore: use path alias import on services

--- src/services/StackbyService.ts
+++ src/services/StackbyService.ts
@@ -1,67 +1,70 @@
-import { cacheOrFetch } from "../utils/cache";
-import { StackbyEndpoint, StackbyDataResponse } from "../types/types";
+import { type StackbyDataResponse, StackbyEndpoint } from "@/types/types.js";
+import { cacheOrFetch } from "@/utils/cache.js";
 import {
-  REDIS_STACKBY_KEYS,
-  STACKBY_BASE_URL,
-  STACKBY_SECRET_KEY,
-} from "../utils/constants";
-import { PROGRESS_CALCULATION_BY_ENTITY } from "../utils/progressCalculationByEntity";
-import { StackbyFilter, StackbyStandardFilter } from "../utils/stackby-filter";
+	REDIS_STACKBY_KEYS,
+	STACKBY_BASE_URL,
+	STACKBY_SECRET_KEY,
+} from "@/utils/constants.js";
+import { PROGRESS_CALCULATION_BY_ENTITY } from "@/utils/progressCalculationByEntity.js";
+import {
+	type StackbyFilter,
+	StackbyStandardFilter,
+} from "@/utils/stackby-filter.js";
 
 export class StackbyService {
-  async fetchStackbyData(
-    endpoint: string,
-    filter?: StackbyFilter | null
-  ): Promise<StackbyDataResponse> {
-    const apiKey: string = STACKBY_SECRET_KEY || "";
-    const uniqueParam: string = `nocache=${Date.now()}`;
-    let url: string = `${STACKBY_BASE_URL}/${endpoint}?${uniqueParam}`;
-    let filterKey = "";
+	async fetchStackbyData(
+		endpoint: string,
+		filter?: StackbyFilter | null,
+	): Promise<StackbyDataResponse> {
+		const apiKey: string = STACKBY_SECRET_KEY || "";
+		const uniqueParam: string = `nocache=${Date.now()}`;
+		let url: string = `${STACKBY_BASE_URL}/${endpoint}?${uniqueParam}`;
+		let filterKey = "";
 
-    if (filter) {
-      url += `&${filter.getStackbyFilterString()}`;
-      filterKey +=
-        filter instanceof StackbyStandardFilter
-          ? `${filter.operator}-${filter.column}-${filter.value}`
-          : `${filter.value}`;
-    }
+		if (filter) {
+			url += `&${filter.getStackbyFilterString()}`;
+			filterKey +=
+				filter instanceof StackbyStandardFilter
+					? `${filter.operator}-${filter.column}-${filter.value}`
+					: `${filter.value}`;
+		}
 
-    return cacheOrFetch(
-      REDIS_STACKBY_KEYS[endpoint as keyof typeof REDIS_STACKBY_KEYS](
-        filterKey ? `${filterKey}` : undefined
-      ),
-      async () => {
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            "x-api-key": apiKey,
-            "Content-Type": "application/json",
-          },
-        });
+		return await cacheOrFetch(
+			REDIS_STACKBY_KEYS[endpoint as keyof typeof REDIS_STACKBY_KEYS](
+				filterKey ? `${filterKey}` : undefined,
+			),
+			async () => {
+				const response = await fetch(url, {
+					headers: {
+						"Content-Type": "application/json",
+						"x-api-key": apiKey,
+					},
+					method: "GET",
+				});
 
-        if (!response.ok) {
-          const text = await response.text();
-          throw new Error(`Stackby API error: ${text}`);
-        }
+				if (!response.ok) {
+					const text = await response.text();
+					throw new Error(`Stackby API error: ${text}`);
+				}
 
-        return response.json();
-      }
-    );
-  }
+				return response.json();
+			},
+		);
+	}
 
-  calculateTotalItems(
-    id: string,
-    endpoint: StackbyEndpoint,
-    items: StackbyDataResponse,
-    topics?: StackbyDataResponse
-  ) {
-    if (endpoint === StackbyEndpoint.THEMES) {
-      return PROGRESS_CALCULATION_BY_ENTITY[endpoint](
-        id,
-        items,
-        topics as StackbyDataResponse
-      );
-    }
-    return PROGRESS_CALCULATION_BY_ENTITY[endpoint](id, items);
-  }
+	calculateTotalItems(
+		id: string,
+		endpoint: StackbyEndpoint,
+		items: StackbyDataResponse,
+		topics?: StackbyDataResponse,
+	) {
+		if (endpoint === StackbyEndpoint.THEMES) {
+			return PROGRESS_CALCULATION_BY_ENTITY[endpoint](
+				id,
+				items,
+				topics as StackbyDataResponse,
+			);
+		}
+		return PROGRESS_CALCULATION_BY_ENTITY[endpoint](id, items);
+	}
 }
