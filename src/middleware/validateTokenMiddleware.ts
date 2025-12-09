992903fb961c4379547f226725713e777a2dc9c0
Peu Silva <peusilva@proton.me>
2025-12-09 16:12:41 -0300

    chore: use path alias import on middlewares

--- src/middleware/validateTokenMiddleware.ts
+++ src/middleware/validateTokenMiddleware.ts
@@ -1,39 +1,49 @@
-import { Request, Response, NextFunction } from "express";
-import { TokenService } from "../services/TokenService";
-import { STATUS_CODE } from "../utils/constants";
-import prisma from "../../client";
+import type { NextFunction, Request, Response } from "express";
+import prisma from "@/root/client.js";
+import { TokenService } from "@/services/TokenService.js";
+import { STATUS_CODE } from "@/utils/constants.js";
 
-export async function validateTokenMiddleware(req: Request, res: Response, next: NextFunction) {
-    const tokenService = new TokenService();
-    const token = req.headers.authorization?.split(" ")[1];
+export async function validateTokenMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	const tokenService = new TokenService();
+	const token = req.headers.authorization?.split(" ")[1];
 
-    if (!token) {
-        return res.status(STATUS_CODE.UNAUTHORIZED).json({ message: "Token was not provided" });
-    }
+	if (!token) {
+		return res
+			.status(STATUS_CODE.UNAUTHORIZED)
+			.json({ message: "Token was not provided" });
+	}
 
-    const extractToken = await tokenService.extractToken(token);
-    const email = extractToken?.email
+	const extractToken = await tokenService.extractToken(token);
+	const email = extractToken?.email;
 
-    try {
-        if (!email) {
-            return res.status(STATUS_CODE.TOKEN_EXPIRED).json({ message: "Token invalid" });
-        }
+	try {
+		if (!email) {
+			return res
+				.status(STATUS_CODE.TOKEN_EXPIRED)
+				.json({ message: "Token invalid" });
+		}
 
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
+		const user = await prisma.user.findUnique({
+			where: {
+				email,
+			},
+		});
 
-        if (!user) {
-            return res
-                .status(STATUS_CODE.NOT_FOUND)
-                .json({ message: "User not found" });
-        }
+		if (!user) {
+			return res
+				.status(STATUS_CODE.NOT_FOUND)
+				.json({ message: "User not found" });
+		}
 
-        req.user = { email, id: +user.id };
-        next();
-    } catch (error) {
-        return res.status(STATUS_CODE.UNAUTHORIZED).json({ message: "Authentication failed" });
-    }
+		req.user = { email, id: +user.id };
+		next();
+	} catch (_error) {
+		return res
+			.status(STATUS_CODE.UNAUTHORIZED)
+			.json({ message: "Authentication failed" });
+	}
 }
